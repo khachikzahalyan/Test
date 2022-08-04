f9b6a8189a861d9650c2230b6a8c45d14ee9b86d
khachikzahalyan <zahalyanxcho@gmail.com>
2022-08-04 20:46:29 +0400

    arajin qayler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+ function App() {
+
+  /* const arr = [
+    [12,45,23],
+    [2,45,1],
+    [1,74,13]
+  ]
+
+   const newArr = arr.flat().reduce(function(aggr, val){
+    return aggr+= val
+  },0)
+ console.log(newArr) */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
